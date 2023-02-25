4763a00a8613d8840d8912b5d30a6fcb02b02abf
John Pierce <jpierce.coder@gmail.com>
2023-02-25 10:06:40 -0600

    MVC Model Examples, Add Bootstrap
    - Created "About" and "Main" controllers and views
    - Added Bootstrap 5.2.3 gem with `bundle install`

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
+# Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
 Rails.application.routes.draw do
   resources :cars
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get "about", to: "about#index"
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root to: "main#index"
 end
